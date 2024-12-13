a043caea99177df5f38324a1a8e1d4aac6849cf4
Nicolas-Munoz-Bacian <nicolasmunoz2001@gmail.com>
2024-12-13 18:20:50 -0300

    Add files via upload

--- src/pages/ModalEditarCard/modal.js
+++ src/pages/ModalEditarCard/modal.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styles from '../../pages/ModalEditarCard/modal.module.css';
 
 function EditModal({ initialData, onClose }) {
     const [formData, setFormData] = useState(initialData);
@@ -10,46 +11,46 @@ function EditModal({ initialData, onClose }) {
 
     const saveChanges = () => {
         // Lógica para guardar los cambios
-        // Esto podría implicar actualizar el estado global o enviar datos a un backend
-        onClose(); // Cierra el modal después de guardar
+        onClose(); // Cierra el modal
     };
 
     const clearChanges = () => {
-        setFormData(initialData); // Restablecer el formulario a los valores iniciales
+        setFormData(initialData); // Restablecer el formulario
     };
 
     return (
-        <div className="modal">
-            <h2>Editar Card</h2>
-            <div>
-                <label>Título:</label>
-                <input name="titulo" value={formData.titulo} onChange={handleChange} />
+        <div className={styles.modal}>
+            <div className={styles.modalContent}>
+                <h2>Editar Card</h2>
+                <div>
+                    <label>Título:</label>
+                    <input name="titulo" value={formData.titulo} onChange={handleChange} />
+                </div>
+                <div>
+                    <label>Categoría:</label>
+                    <select name="categoria" value={formData.categoria} onChange={handleChange}>
+                        <option value="opcion1">Opción 1</option>
+                        <option value="opcion2">Opción 2</option>
+                    </select>
+                </div>
+                <div>
+                    <label>Imagen:</label>
+                    <input name="capa" value={formData.capa} onChange={handleChange} />
+                </div>
+                <div>
+                    <label>Video URL:</label>
+                    <input name="video" value={formData.video} onChange={handleChange} />
+                </div>
+                <div>
+                    <label>Descripción:</label>
+                    <textarea name="descripcion" value={formData.descripcion} onChange={handleChange} />
+                </div>
+                <button onClick={saveChanges}>Guardar Cambios</button>
+                <button onClick={clearChanges}>Limpiar Cambios</button>
+                <button onClick={onClose}>Cerrar</button>
             </div>
-            <div>
-                <label>Categoría:</label>
-                <select name="categoria" value={formData.categoria} onChange={handleChange}>
-                    <option value="opcion1">Front-End</option>
-                    <option value="opcion2">BackEnd</option>
-                    <option value="opcion2">Innovación y gestion</option>
-                </select>
-            </div>
-            <div>
-                <label>Imagen:</label>
-                <input name="capa" value={formData.capa} onChange={handleChange} />
-            </div>
-            <div>
-                <label>Video:</label>
-                <input name="video" value={formData.video} onChange={handleChange} />
-            </div>
-            <div>
-                <label>Descripción:</label>
-                <textarea name="descripcion" value={formData.descripcion} onChange={handleChange} />
-            </div>
-            <button onClick={saveChanges}>Guardar Cambios</button>
-            <button onClick={clearChanges}>Limpiar Cambios</button>
-            <button onClick={onClose}>Cerrar</button>
         </div>
     );
 }
 
-export default EditModal
+export default EditModal;
