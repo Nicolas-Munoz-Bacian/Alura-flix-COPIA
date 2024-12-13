0483638e666e2c12cb5fa15f326c641f20e67562
Nicolas-Munoz-Bacian <nicolasmunoz2001@gmail.com>
2024-12-13 17:33:36 -0300

    Add files via upload

--- src/components/Pie/index.js
+++ src/components/Pie/index.js
@@ -1,16 +1,11 @@
-import styles from './Pie.module.css'
-import logo from './Sello Nicolás.png'
-import logoalura from './logo-Main.png'
+import styles from "./Pie.module.css"
+import logo from "./logo-alura.png"
 
-function Pie() {
-    return (
-        <footer className={styles.rodape}>
-            <h2>Desarrollado por Nicolás Muñoz Bacián ©Diseñador Gráfico.</h2>
-            <img src={logo} alt="Logo" className={styles.logo} />
-            <img src={logoalura} alt="Logoalura" className={styles.logo} />
-            
-        </footer>
-    )
+function Pie(){
+return(<footer className={styles.pie}>
+<h2>Desarrollado por <img src={logo} alt="Alura"/></h2>
+
+</footer>)
+
 }
-
-export default Pie;
+export default Pie
