4e9d3903cae4a836a2744001e08b98556b372290
Nicolas-Munoz-Bacian <nicolasmunoz2001@gmail.com>
2024-12-18 11:42:23 -0300

    Add files via upload

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -4,6 +4,9 @@ import Banner from "../../components/Banner";
 import Titulo from "../../components/Titulo";
 import NotFound from "../../pages/NotFound";
 import styles from "./Player.module.css";
+import frontend from "../../pages/inicio/front end.png";
+import backend from "../../pages/inicio/back end.png";
+import innovacionYgestion from "../inicio/innovación y gestión.png";
 
 function Player() {
     const [video, setVideo] = useState(null);
@@ -15,8 +18,7 @@ function Player() {
             .then(data => {
                 if (data.length > 0) {
                     const videoData = data[0];
-                    // Ajustar la URL para asegurar que es embebible
-                    const videoURL = videoData.link.replace("watch?v=", "embed/");
+                    const videoURL = convertToEmbedURL(videoData.video); // Usa videoData.video
                     setVideo({ ...videoData, link: videoURL });
                 } else {
                     setVideo(null);
@@ -29,7 +31,7 @@ function Player() {
     }, [parametros.id]);
 
     if (!video) return <NotFound />;
-    
+
     return (
         <>
             <Banner img="player" color="#58B9AE" />
@@ -37,18 +39,27 @@ function Player() {
                 <h1>Player</h1>
             </Titulo>
             <section className={styles.container}>
-                <iframe 
+                <iframe
                     width="100%"
-                    height="80vh" 
-                    src={video.link} 
-                    title={video.titulo} 
-                    frameBorder="0" 
+                    height="80vh"
+                    src={video.link} // Usa video.link aquí
+                    title={video.titulo}
+                    frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                 />
             </section>
         </>
     );
+}
+
+function convertToEmbedURL(youtubeURL) {
+    // Asegúrate de que la URL de YouTube tenga el formato correcto
+    if (!youtubeURL.includes('watch?v=')) {
+        console.error('URL de YouTube inválida:', youtubeURL);
+        return ''; // Retorna una cadena vacía si la URL es inválida
+    }
+    return youtubeURL.replace('watch?v=', 'embed/');
 }
 
 export default Player;
