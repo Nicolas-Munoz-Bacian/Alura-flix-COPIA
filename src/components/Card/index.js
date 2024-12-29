30e30cfca08eebf0437e0c573de03172c446579f
Nicolas-Munoz-Bacian <nicolasmunoz2001@gmail.com>
2024-12-29 17:52:48 -0300

    Add files via upload

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,29 +6,28 @@ import iconNoFavorito from "../../components/Card/iconNoFavorito.png";
 import EditModal from "../../pages/ModalEditarCard/modal";
 import { Link } from 'react-router-dom';
 
+
 function Card({ id, capa, titulo, descripcion, video, onDelete, onSave, onClear, imagen, onPlay }) {
     const { favorito, agregarFavorito } = useFavoritosContext();
     const [showModal, setShowModal] = useState(false);
     const isFavorito = favorito.some(fav => fav.id === id);
     const icon = isFavorito ? iconFavorito : iconNoFavorito;
 
-    // Manejar la edición del video
     const handleEdit = () => {
         setShowModal(true);
     };
 
-    // Manejar la eliminación del video
     const handleDelete = () => {
         if (onDelete) onDelete(id); // Llama a la función de eliminación pasada como prop
     };
 
-    // Formatear URL de YouTube
     const formatYouTubeURL = (url) => {
-        if (!url) return ''; // Si no hay URL, retornar vacío
+        // Asegúrate de que las URLs de YouTube sean válidas
+        if (!url) return ''; // Verifica si la URL está vacía
         if (url.startsWith('http')) {
             return url; // Ya está bien formada
-        } else if (url.includes('watch?v=')) { // Formación estándar para URLs de YouTube
-            return `https://www.youtube.com/watch?v=${url.split('watch?v=')[1]}`;
+        } else if (url.includes('watch?v=')) {
+            return `https://www.youtube.com/watch?v=${url.split('watch?v=')[1]}`; // Formación estándar para URLs de YouTube
         } else if (url.includes('youtu.be/')) {
             const videoId = url.split('youtu.be/')[1]; // Extrae el ID de la URL corta
             return `https://www.youtube.com/watch?v=${videoId}`;
@@ -37,33 +36,27 @@ function Card({ id, capa, titulo, descripcion, video, onDelete, onSave, onClear,
         }
     };
 
-    // Manejar la redirección del video
-    const handleRedirect = () => {
+    // Manejar la reproducción del video
+    const handlePlayVideo = () => {
         const formattedVideoUrl = formatYouTubeURL(video);
+        // Verifica si la URL formateada es válida
         if (formattedVideoUrl) {
-            window.open(formattedVideoUrl, "_blank"); // Redirigir al reproductor
+            if (onPlay) {
+                onPlay(formattedVideoUrl); // Llama a onPlay con el URL formateado
+            } else {
+                window.open(formattedVideoUrl, "_blank"); // Redirige a YouTube
+            }
         } else {
-            console.error('El video no se pudo formatear correctamente.'); // Manejo de error
+            console.error('Formato de video no válido:', video); // Manejo de errores
         }
     };
 
-    // Manejar la reproducción del video
-    const handlePlayVideo = (event) => {
-        event.preventDefault(); // Prevenir el comportamiento predeterminado del enlace
-        const formattedVideoUrl = formatYouTubeURL(video);
-
-        // Llama a onPlay si está definido, de lo contrario redirige
-        if (onPlay) {
-            onPlay(formattedVideoUrl);
+    const handleRedirect = () => {
+        if (video) {
+            const formattedVideoUrl = formatYouTubeURL(video);
+            window.open(formattedVideoUrl, "_blank"); // Redirigir a YouTube
         } else {
-            handleRedirect(); // Llamada a handleRedirect
-        }
-
-        // Añadir verificación para ver cómo se está formateando el URL
-        console.log('Formatted Video URL:', formattedVideoUrl);
-        
-        if (!formattedVideoUrl) {
-            console.error('El video no se pudo formatear correctamente.');
+            console.error('No hay URL de video para redirigir');
         }
     };
 
@@ -74,7 +67,7 @@ function Card({ id, capa, titulo, descripcion, video, onDelete, onSave, onClear,
                     src={capa} 
                     alt={titulo} 
                     className={styles.imagen}
-                    onClick={handleRedirect}
+                    onClick={handlePlayVideo} // Llama a la función para reproducir el video
                 />
                 <h2>{titulo}</h2>
             </Link>
@@ -82,7 +75,7 @@ function Card({ id, capa, titulo, descripcion, video, onDelete, onSave, onClear,
                 src={icon} 
                 alt="Icono favorito"
                 className={styles.favorito}
-                onClick={() => agregarFavorito({ id, titulo, capa })}
+                onClick={() => agregarFavorito({ id, titulo, capa })} // Manejar favoritos
             />
             <button onClick={handleEdit} className={styles.button}>
                 Editar
